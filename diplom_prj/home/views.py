451274df21d43d7be64ebab7ce32dcd8eb9d6bb7
Gusev Vova <volkagu@yandex.ru>
2023-05-20 17:42:28 +0300

    link database with home-template

--- diplom_prj/home/views.py
+++ diplom_prj/home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, HttpResponse
 
+from .models import *
+
 def index(request):
+  tasks = Tasks.objects.all()
+  
   return render(request, 'home/index.html', {
+    'tasks': tasks,
     'auth':True,
+
   })
