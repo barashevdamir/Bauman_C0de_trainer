ee8bc498af14643a44e864f9559a6b7557920842
Gusev Vova <volkagu@yandex.ru>
2023-05-27 17:37:02 +0300

    link filters

--- diplom_prj/home/static/home/js/main.js
+++ diplom_prj/home/static/home/js/main.js
@@ -1,5 +1,10 @@
-console.log('test')
 let intervalId;
+
+
+
+
+
+
 document.querySelectorAll('.filter__dropdown-btn').forEach(e=>{
   e.addEventListener('click', e =>{
     const menu = e.currentTarget.dataset.path;
@@ -28,4 +33,123 @@ document.querySelectorAll('.filter__dropdown-btn').forEach(e=>{
       }
     })
   })
+})
+
+
+
+
+document.querySelectorAll('.order_by').forEach(e=>{
+  e.addEventListener('click', e=>{
+
+    let url = window.location;
+    let params = window
+    .location
+    .search
+    .replace('?','')
+    .split('&')
+    
+    if (params != 0) {
+      params.forEach((item, i) => {
+        if (item.slice(0,9) == 'order_by=') {
+          params.splice(i,1)
+        } 
+
+      })
+      params.push('order_by=' + e.target.id)
+      url = url.origin + url.pathname + '?' + params.sort().reverse().join('&')
+    } else if (params == 0) {
+      url = url.origin + url.pathname + '?order_by=' + e.target.id
+    }
+
+    window.location.replace(url)
+  })
+})
+
+
+document.querySelectorAll('.language').forEach(e=>{
+  e.addEventListener('click', e=>{
+
+    let url = window.location;
+    let params = window
+    .location
+    .search
+    .replace('?','')
+    .split('&')
+    
+    if (params != 0) {
+      params.forEach((item, i) => {
+        if (item.slice(0,9) == 'language=') {
+          params.splice(i,1)
+        } 
+
+      })
+      params.push('language=' + e.target.id)
+      url = url.origin + url.pathname + '?' + params.sort().reverse().join('&')
+    } else if (params == 0) {
+      url = url.origin + url.pathname + '?language=' + e.target.id
+    }
+
+    window.location.replace(url)
+  })
+})
+
+
+document.querySelectorAll('.level').forEach(e=>{
+  e.addEventListener('click', e=>{
+    let url = window.location;
+    let params = window
+    .location
+    .search
+    .replace('?','')
+    .split('&')
+    
+    if (params != 0) {
+      params.forEach((item, i) => {
+        if (item.slice(0,6) == 'level=') {
+          params.splice(i,1)
+        } 
+
+      })
+      params.push('level=' + e.target.id)
+      url = url.origin + url.pathname + '?' + params.sort().reverse().join('&')
+    } else if (params == 0) {
+      url = url.origin + url.pathname + '?level=' + e.target.id
+    }
+
+    window.location.replace(url)
+  })
+})
+
+
+document.querySelectorAll('.tag').forEach(e=>{
+  e.addEventListener('click', e=>{
+
+    let url = window.location;
+    let params = window
+    .location
+    .search
+    .replace('?','')
+    .split('&')
+    
+    if (params != 0) {
+      params.forEach((item, i) => {
+        if (item.slice(0,4) == 'tag=') {
+          params.splice(i,1)
+        } 
+
+      })
+      params.push('tag=' + e.target.id)
+      url = url.origin + url.pathname + '?' + params.sort().reverse().join('&')
+    } else if (params == 0) {
+      url = url.origin + url.pathname + '?tag=' + e.target.id
+    }
+
+    window.location.replace(url)
+  })
+})
+
+document.querySelector('.filter__clear-btn').addEventListener('click', e => {
+  let url = window.location;
+  url = url.origin + url.pathname + ''
+  window.location.replace(url)
 })
--- diplom_prj/home/views.py
+++ diplom_prj/home/views.py
@@ -4,9 +4,44 @@ from .models import *
 
 def index(request):
   tasks = Tasks.objects.all()
+
+  if request.GET:
+    get_turple = request.GET
+
+    if request.GET.get('order_by', False):
+      tasks = tasks.order_by(get_turple['order_by'])
+
+    if request.GET.get('level', False):
+      tasks = tasks.filter(level=get_turple['level'])
+
+    if request.GET.get('language', False):
+      tasks_arr = []
+      for task in tasks:
+        tmp_arr = task.languages.split()
+        for el in tmp_arr:
+          if el == get_turple['language']:
+            tasks_arr.append(task.name)
+      tasks_str = '\', \''.join(tasks_arr)
+      tasks = Tasks.objects.raw('SELECT * FROM public.home_tasks WHERE name IN (\'' + tasks_str + '\') ORDER BY id ASC')
+
+    if request.GET.get('tag', False):
+      tasks_arr = []
+      for task in tasks:
+        tmp_arr = task.tags.split()
+        for el in tmp_arr:
+          if el == get_turple['tag']:
+            tasks_arr.append(task.name)
+      tasks_str = '\', \''.join(tasks_arr)
+      tasks = Tasks.objects.raw('SELECT * FROM public.home_tasks WHERE name IN (\'' + tasks_str + '\') ORDER BY id ASC')
+
+    if request.GET.get('language', False):
+      if get_turple['language'] == 'All':
+        tasks = Tasks.objects.all()
+
+    print(tasks)
+
   
   return render(request, 'home/index.html', {
     'tasks': tasks,
     'auth':True,
-
   })
