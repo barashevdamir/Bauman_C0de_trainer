18f7beb182ed632cd0ae33d6a0cd930ba82f63ac
Alisa <danilka.frolov21@gmail.com>
2023-06-02 12:04:06 +0300

    tests models changed

--- diplom_prj/diplom/settings.py
+++ diplom_prj/diplom/settings.py
@@ -89,12 +89,8 @@ WSGI_APPLICATION = 'diplom.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "student_barashev_01",
-        "USER": "student_barashev_01",
-        "PASSWORD": "qwerty123",
-        "HOST": "dc-webdev.bmstu.ru",
-        "PORT": "8080",
+         'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- diplom_prj/tests/models.py
+++ diplom_prj/tests/models.py
@@ -1,15 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Test(models.Model):
-	author = models.ForeignKey(User, on_delete=models.DO_NOTHING, default=None) #а нужно ли оно?
-	title = models.CharField(max_length=256, default='') 
-	created_at = models.DateTimeField(auto_now_add=True)
+
+	class Status(models.TextChoices):
+		DRAFT = 'DF', 'Draft'
+		PUBLISHED = 'PB', 'Published'
+
+	author = models.ForeignKey(
+		User, 
+		on_delete=models.SET_DEFAULT, 
+		default=None,
+		related_name='tests')
+	title = models.CharField(max_length=256, default='')
+	#slug? 
 	times_taken = models.IntegerField(default=0, editable=False)
-	experience = models.PositiveSmallIntegerField(default=0) #эвивалетнт lvl в задачках, нужно для таблицы лидеров, хотя может тесты и не достойны там учитываться
+	experience = models.PositiveSmallIntegerField(default=0) #эквивалетнт lvl в задачках, нужно для таблицы лидеров, хотя может тесты и не достойны там учитываться
 	prog_language = models.CharField(max_length=32, default='') #может сделать список для выбора языка? есть вариант сделать этот список общим для проекта и тащить его сюда и в модель задачек
 	#может добавить описание?
-	#теги?
+	#теги? нужно обновить пип лист
+	publish = models.DateTimeField(default=timezone.now)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	status = models.CharField(
+		max_length=2,
+		choices=Status.choices,
+		default=Status.DRAFT,
+		blank=False
+	)
 
 	@property
 	def question_count(self):
@@ -17,21 +36,21 @@ class Test(models.Model):
 	
 	class Meta:
 		verbose_name_plural = "Tests"
-		ordering = ['id']
+		ordering = ['-publish']
+		indexes = [
+			models.Index(fields=['-publish'])
+		]
 
 	def __str__(self):
 		return f'{self.title}, {self.author}'
 
 class Question(models.Model):
 	
-	SINGLE = 'SC'
-	MULTIPLE = 'MC'
-	WRITTEN = 'WR'
-	ANSWER_TYPE_CHOICES = [
-		(SINGLE, 'single choice'),
-		(MULTIPLE, 'multiple choice'),
-		(WRITTEN,'written')
-	]
+	class AnswerType(models.TextChoices):
+		SINGLE = 'SC', 'single choice'
+		MULTIPLE = 'MC', 'multiple choice'
+		WRITTEN = 'WR', 'written'
+	
 
 	test = models.ForeignKey(
 		Test, 
@@ -41,8 +60,8 @@ class Question(models.Model):
 	prompt = models.CharField(max_length=256, default='')
 	answer_type = models.CharField(
         max_length=2,
-        choices=ANSWER_TYPE_CHOICES,
-        default=SINGLE,
+        choices=AnswerType.choices,
+        default=AnswerType.SINGLE,
 		blank=False
     )
 	#может нужна какая-то функция для проверки ответа соответсвию типу ответа?
