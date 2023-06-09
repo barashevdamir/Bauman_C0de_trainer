e73de3622b62b8c6a3142e1e1b68a87dc4aac580
Alisa <danilka.frolov21@gmail.com>
2023-06-10 00:09:29 +0300

    test template created

--- diplom_prj/tests/views.py
+++ diplom_prj/tests/views.py
@@ -25,13 +25,32 @@ def test(request, id):
     id=id,
     status=Test.Status.DRAFT
   )
-  #questions = []
-  #for q in test.questions :
-
+  questions = []
+  for q in test.get_questions():
+    answers = []
+    codes =[]
+    for c in q.get_codes():
+      codes.append(c.text)
+    for a in q.get_answers():
+      answers.append(a.text)
+    questions.append({
+      'prompt': q.prompt,
+      'answer_type': q.answer_type,
+      'codes': codes, 
+      'answers': answers
+    })
+  # paginator = Paginator(questions, 1)
+  # page_number = request.GET.get('question')
+  # try:
+  #   questions = paginator.page(page_number)
+  # except PageNotAnInteger:
+  #   questions = paginator.page(1)
+  # except EmptyPage:
+  #   questions = paginator.page(paginator.num_pages)
   return render(
     request,
     'tests/test.html',
-    {'test': test}
+    {'test': test, 'questions': questions}
   )
 
 def result(request):
