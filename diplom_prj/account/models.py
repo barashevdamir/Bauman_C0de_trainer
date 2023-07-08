6f5b525336db3eb8ff8625cde733509db7613c47
Дамир Барашев <barashev-01@mail.ru>
2023-07-08 21:52:17 +0300

    bug fixed at registration

--- diplom_prj/account/models.py
+++ diplom_prj/account/models.py
@@ -23,12 +23,3 @@ class Profile(models.Model):
     def __str__(self):
         return f'Profile of {self.user.username}'
 
-    def save(self, *args, **kwargs):
-        super().save()
-
-        img = Image.open(self.avatar.path)
-
-        if img.height > 100 or img.width > 100:
-            new_img = (100, 100)
-            img.thumbnail(new_img)
-            img.save(self.avatar.path)
--- diplom_prj/tasks/urls.py
+++ diplom_prj/tasks/urls.py
@@ -5,5 +5,4 @@ from .views import *
 urlpatterns = [
   path('', taskspage, name='tasks'),
   path('task-id<int:id>/', task, name='task'),
-  path('task-id<int:id>/check', task_check, name='task_check'),
 ]
