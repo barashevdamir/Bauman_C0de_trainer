f23fb4d7f3b84bd9a32034b425fbed3471225d3a
Дамир Барашев <barashev-01@mail.ru>
2023-07-11 17:52:51 +0300

    upgraded training page view for js, added js task description, test, solution files, training page views got the code where some parts are refactored into separate functions for better modularity and readability

--- diplom_prj/tasks/urls.py
+++ diplom_prj/tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
+from . import views
 from .views import *
 
 urlpatterns = [
   path('', taskspage, name='tasks'),
   path('task-id<int:id>/', task, name='task'),
+  path('get_solution/', views.get_solution, name='get_solution'),
 ]
--- diplom_prj/tasks/views.py
+++ diplom_prj/tasks/views.py
@@ -62,211 +62,216 @@ def get_file_content(request):
     return JsonResponse({'content': content})
   else:
     return JsonResponse({'error': 'File not found'}, status=404)
+def get_solution(request):
+  task_id = request.GET.get('task_id', '')
+  language = request.GET.get('language', '')
+
+  try:
+    task = Tasks.objects.get(id=task_id)
+    task_language = TaskLanguage.objects.get(task=task, prog_language=language)
+    solution_code = task_language.solution_file.read().decode()
+    return JsonResponse({'content': solution_code})
+  except Exception as e:
+    return JsonResponse({'error': str(e)}, status=500)
+
 
 @login_required
 def task(request, id):
-
-  task = get_object_or_404(
-    Tasks,
-    id=id,
-    status=Status.PUBLISHED
-  )
-
+  task = get_object_or_404(Tasks, id=id, status=Status.PUBLISHED)
   context = {'task': task}
 
   if request.POST and request.headers.get('x-requested-with') == 'XMLHttpRequest':
-
     form_data = dict(request.POST.lists())
     language = str.lower(form_data['language'][0])
     code = form_data['code'][0]
     file_name = 'task' + str(task.id)
 
+    if language == 'py':
+      handle_python_task(request, task, code, file_name)
+    elif language == 'js':
+      handle_javascript_task(request, task, code, file_name)
+    elif language in ['c', 'cpp']:
+      handle_c_cpp_task(request, task, code, file_name)
 
-    if language =='py':
-
-      if request.user.is_authenticated:
-        directory = f'{settings.MEDIA_ROOT}/tasks/task_id{task.id}/{request.user.username}'
-        if not os.path.exists(directory):
-          os.makedirs(directory)
-        # Save the code in a .py file
-        with open(f"{directory}/{file_name}.py", "w") as file:
-          file.write(code)
-
-      epicbox.configure(
-        profiles=[
-          epicbox.Profile('python', 'python')
-        ]
-      )
-
-      # Создаем временную директорию для работы с файлами
-      temp_dir = tempfile.mkdtemp()
-
-      # Подготовка кода тестов
-
-      task_language = TaskLanguage.objects.get(task=task, prog_language=ProgLanguage.PYTHON)
-
-      pytest_code = task_language.test_file.read()
-    
-      # test_code = pytest_code
-      test_code =  bytes(code, 'utf-8') + b'\n\n'+ pytest_code
+  handle_solution_code_and_output(request, task, context)
+  return render(request, 'tasks/training.html', context)
 
 
-      # Настройка Epicbox
+def handle_python_task(request, task, code, file_name):
+  if request.user.is_authenticated:
+    directory = f'{settings.MEDIA_ROOT}/tasks/task_id{task.id}/{request.user.username}'
+    if not os.path.exists(directory):
+      os.makedirs(directory)
+    # Save the code in a .py file
+    with open(f"{directory}/{file_name}.py", "w") as file:
+      file.write(code)
 
-      epicbox.configure(profiles=[
-          epicbox.Profile('python', 'python')
-      ])
+  # Epicbox configuration
+  epicbox.configure(profiles=[epicbox.Profile('python', 'python')])
 
-      files = [{'name': 'test_code.py', 'content': test_code}]
-      limits = {'cputime': 1, 'memory': 64}
+  # Create a temporary directory for file operations
+  temp_dir = tempfile.mkdtemp()
 
-      # Создание контейнера
+  # Prepare the test code
+  task_language = TaskLanguage.objects.get(task=task, prog_language=ProgLanguage.PYTHON)
+  pytest_code = task_language.test_file.read()
+  test_code = bytes(code, 'utf-8') + b'\n\n' + pytest_code
 
-      container = epicbox.run('python', 'python3 -m unittest test_code.py',
-                              files=files,
-                              limits=limits)
+  files = [{'name': 'test_code.py', 'content': test_code}]
+  limits = {'cputime': 1, 'memory': 64}
+
+  # Create a container
+  container = epicbox.run('python', 'python3 -m unittest test_code.py',
+                          files=files,
+                          limits=limits)
+
+  # Copy the container results to the temporary directory
+  stdout_path = os.path.join(temp_dir, 'stdout')
+  stderr_path = os.path.join(temp_dir, 'stderr')
+
+  with open(stdout_path, 'wb') as stdout_file:
+    stdout_file.write(container['stdout'])
+
+  with open(stderr_path, 'wb') as stderr_file:
+    stderr_file.write(container['stderr'])
+
+  # Evaluate the test results
+  request.session['code'] = code
+
+  passed = False
+  exp_gain = 0
+
+  if container['exit_code'] == 0:
+    passed = True
+    exp_gain = task.level * 3
+    if request.user.is_authenticated:
+      with open(f"{directory}/{file_name}_result.txt", "w") as file:
+        file.write("Tests passed successfully.")
+  else:
+    if request.user.is_authenticated:
+      with open(f"{directory}/{file_name}_result.txt", "w") as file:
+        file.write("Tests failed.")
+
+  result = Result(user=request.user, task=task, code=code, file_name=file_name,
+                  prog_language=ProgLanguage.PYTHON, passed=passed, exp_gain=exp_gain)
+  result.save()
+
+  # Remove the temporary directory
+  shutil.rmtree(temp_dir)
 
 
-      # Копируем результаты из контейнера во временную директорию
+def handle_javascript_task(request, task, code, file_name):
+  if request.user.is_authenticated:
+    directory = f'{settings.MEDIA_ROOT}/tasks/task_id{task.id}/{request.user.username}'
+    if not os.path.exists(directory):
+      os.makedirs(directory)
+    # Save the code in a .js file
+    with open(f"{directory}/{file_name}.js", "w") as file:
+      file.write(code)
 
-      stdout_path = os.path.join(temp_dir, 'stdout')
-      stderr_path = os.path.join(temp_dir, 'stderr')
+  # Epicbox configuration for JavaScript
+  epicbox.configure(profiles=[epicbox.Profile('nodejs', 'nodejs')])
 
-      with open(stdout_path, 'wb') as stdout_file:
-        stdout_file.write(container['stdout'])
+  # Create a temporary directory for file operations
+  temp_dir = tempfile.mkdtemp()
 
-      with open(stderr_path, 'wb') as stderr_file:
-        stderr_file.write(container['stderr'])
+  # Prepare the test code
+  task_language = TaskLanguage.objects.get(task=task, prog_language=ProgLanguage.JAVASCRIPT)
+  test_code = task_language.test_file.read()
 
-      # Оценка результата pytest
+  # Concatenate the code and the test code
+  test_code = bytes(code, 'utf-8') + b'\n\n' + test_code
 
-      request.session['code'] = code
+  files = [{'name': 'test_code.js', 'content': test_code}]
+  limits = {'cputime': 1, 'memory': 64}
 
-      passed = False
-      exp_gain = 0
+  # Create a container
+  container = epicbox.run('nodejs', 'node test_code.js',
+                          files=files,
+                          limits=limits)
 
-      if container['exit_code'] == 0:
-        passed = True
-        exp_gain = task.level*3
-        if request.user.is_authenticated:
-          with open(f"{directory}/{file_name}_result.txt", "w") as file:
-            file.write("Тесты пройдены успешно.")
-      else:
-        if request.user.is_authenticated:
-          with open(f"{directory}/{file_name}_result.txt", "w") as file:
-            file.write("Тесты провалены.")
+  # Copy the container results to the temporary directory
+  stdout_path = os.path.join(temp_dir, 'stdout')
+  stderr_path = os.path.join(temp_dir, 'stderr')
 
-      result = Result(user=request.user, task=task, code=code, file_name=file_name, prog_language=ProgLanguage.PYTHON, passed=passed, exp_gain=exp_gain)
-      result.save()
+  with open(stdout_path, 'wb') as stdout_file:
+    stdout_file.write(container['stdout'])
 
-      # Удаляем временную директорию
+  with open(stderr_path, 'wb') as stderr_file:
+    stderr_file.write(container['stderr'])
 
-      shutil.rmtree(temp_dir)
+  # Evaluate the test results
+  request.session['code'] = code
 
-    elif language == "js":
+  passed = False
 
-      if request.user.is_authenticated:
-        directory = f'{settings.MEDIA_ROOT}/tasks/task_id{task.id}/{request.user.username}'
-        if not os.path.exists(directory):
-          os.makedirs(directory)
-        # Save the code in a .js file
-        with open(f"{directory}/{file_name}.js", "w") as file:
-          file.write(code)
+  if container['exit_code'] == 0:
+    passed = True
+    if request.user.is_authenticated:
+      with open(f"{directory}/{file_name}_result.txt", "w") as file:
+        file.write("Tests passed successfully.")
+  else:
+    if request.user.is_authenticated:
+      with open(f"{directory}/{file_name}_result.txt", "w") as file:
+        file.write("Tests failed.")
 
-      # Configure Epicbox for JavaScript
-      epicbox.configure(
-        profiles=[
-          epicbox.Profile('nodejs', 'nodejs')
-        ]
-      )
+  result = Result(user=request.user, task=task, code=code, file_name=file_name,
+                  prog_language=ProgLanguage.JAVASCRIPT, passed=passed)
+  result.save()
 
-      # Create a temporary directory for file operations
-      temp_dir = tempfile.mkdtemp()
+  # Remove the temporary directory
+  shutil.rmtree(temp_dir)
 
-      # Prepare the test code
-      task_language = TaskLanguage.objects.get(task=task, prog_language=ProgLanguage.JAVASCRIPT)
-      test_code = task_language.test_file.read()
 
-      # Concatenate the code and the test code
-      test_code = bytes(code, 'utf-8') + b'\n\n' + test_code
+def handle_c_cpp_task(request, task, code, file_name):
+  # Handle C/C++ tasks
+  pass
 
-      # Configure Epicbox for JavaScript
-      epicbox.configure(
-        profiles=[
-          epicbox.Profile('nodejs', 'nodejs')
-        ]
-      )
 
-      files = [{'name': 'test_code.js', 'content': test_code}]
-      limits = {'cputime': 1, 'memory': 64}
-
-      # Create a container
-      container = epicbox.run('nodejs', 'node test_code.js',
-                              files=files,
-                              limits=limits)
-
-      # Copy the container results to the temporary directory
-      stdout_path = os.path.join(temp_dir, 'stdout')
-      stderr_path = os.path.join(temp_dir, 'stderr')
-
-      with open(stdout_path, 'wb') as stdout_file:
-        stdout_file.write(container['stdout'])
-
-      with open(stderr_path, 'wb') as stderr_file:
-        stderr_file.write(container['stderr'])
-
-      # Evaluate the test results
-      request.session['code'] = code
-
-      passed = False
-
-      if container['exit_code'] == 0:
-        passed = True
-        if request.user.is_authenticated:
-          with open(f"{directory}/{file_name}_result.txt", "w") as file:
-            file.write("Tests passed successfully.")
-      else:
-        if request.user.is_authenticated:
-          with open(f"{directory}/{file_name}_result.txt", "w") as file:
-            file.write("Tests failed.")
-
-      result = Result(user=request.user, task=task, code=code, file_name=file_name,
-                      prog_language=ProgLanguage.JAVASCRIPT, passed=passed)
-      result.save()
-
-      # Remove the temporary directory
-      shutil.rmtree(temp_dir)
-
-    elif language == "c":
-        pass
-
-    elif language == "cpp":
-        pass
-
-  # проверка, есть ли код и выходные данные
-
+def handle_solution_code_and_output(request, task, context):
   file_name = 'task' + str(task.id)
   directory = f'{settings.MEDIA_ROOT}/tasks/task_id{task.id}/{request.user.username}'
 
-  try:
-    with open(f"{directory}/{file_name}.py", "r") as file:
-      code = file.read()
-    context['code'] = code
-  except Exception as e:
-    print(f"Error reading code file: {e}")
-    context['code'] = ''
+  if task.languages.filter(prog_language=ProgLanguage.PYTHON).exists():
+    # Retrieve Python solution, code, and output
+    task_language = TaskLanguage.objects.get(task=task, prog_language=ProgLanguage.PYTHON)
+    tasks_solution_code = task_language.solution_file.read()
+    context['solution'] = tasks_solution_code.decode()
 
-  try:
-    with open(f"{directory}/{file_name}_result.txt", "r") as file:
-      output = file.read()
-    context['output'] = output
-  except Exception as e:
-    print(f"Error reading output file: {e}")
-    context['output'] = ''
+    try:
+      with open(f"{directory}/{file_name}.py", "r") as file:
+        code = file.read()
+      context['code'] = code
+    except Exception as e:
+      print(f"Error reading code file: {e}")
+      context['code'] = ''
 
-  task_language = TaskLanguage.objects.get(task=task, prog_language=ProgLanguage.PYTHON)
+    try:
+      with open(f"{directory}/{file_name}_result.txt", "r") as file:
+        output = file.read()
+      context['output'] = output
+    except Exception as e:
+      print(f"Error reading output file: {e}")
+      context['output'] = ''
 
-  tasks_solution_code = task_language.solution_file.read()
-  context['solution'] = tasks_solution_code.decode()
+  elif task.languages.filter(prog_language=ProgLanguage.JAVASCRIPT).exists():
+    # Retrieve JavaScript solution, code, and output
+    task_language = TaskLanguage.objects.get(task=task, prog_language=ProgLanguage.JAVASCRIPT)
+    tasks_solution_code = task_language.solution_file.read()
+    context['solution'] = tasks_solution_code.decode()
 
-  return render(request, 'tasks/training.html',  context)
+    try:
+      with open(f"{directory}/{file_name}.js", "r") as file:
+        code = file.read()
+      context['code'] = code
+    except Exception as e:
+      print(f"Error reading code file: {e}")
+      context['code'] = ''
+
+    try:
+      with open(f"{directory}/{file_name}_result.txt", "r") as file:
+        output = file.read()
+      context['output'] = output
+    except Exception as e:
+      print(f"Error reading output file: {e}")
+      context['output'] = ''
