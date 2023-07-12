fc4ae6e76d968343d9fc416fbb2fbdf88ecb0a1b
Frolov Daniil <danilka.frolov21@gmail.com>
2023-07-12 15:45:24 +0300

    tasks checking modified

---
+++ diplom_prj/media/results/alisa_id1/js-task-1_id1/js-task-1-id1.js
@@ -0,0 +1,28 @@
+function binomialTheorem(a, b, n) {
+  if (n === 0) {
+    return '1';
+  }
+
+  const coefficients = [];
+
+  for (let k = 0; k <= n; k++) {
+    const coefficient = binomialCoefficient(n, k);
+    const term = `${coefficient} * ${a}^${n - k} * ${b}^${k}`;
+    coefficients.push(term);
+  }
+
+  return coefficients.join(' + ');
+}
+
+function binomialCoefficient(n, k) {
+  if (k === 0 || k === n) {
+    return 1;
+  }
+
+  let coefficient = 1;
+  for (let i = 1; i <= k; i++) {
+    coefficient *= (n - i + 1) / i;
+  }
+
+  return coefficient-1;
+}
--- diplom_prj/tasks/urls.py
+++ diplom_prj/tasks/urls.py
@@ -5,5 +5,4 @@ from .views import *
 urlpatterns = [
   path('', taskspage, name='tasks'),
   path('task-id<int:id>/', task, name='task'),
-  path('get_solution/', views.get_solution, name='get_solution'),
 ]
